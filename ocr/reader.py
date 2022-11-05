0ac9fe86faaf665448a22a61c7a54e722034ca67
AtmegaBuzz <swapnilshinde9382@gmail.com>
2022-11-05 13:09:02 +0530

    Reformatting | Code cleanup

--- ocr/reader.py
+++ ocr/reader.py
@@ -10,38 +10,20 @@ def read_data(filepath):
         try:
             int(i[1])
         except:
-            # print(i[1])
             fresult.append(i[1].lower())
 
     def clean_list():
         fresult2=[]
         temp = []
-        # for i in fresult:
-            # chap_keyword = i.split(' ')
-            # if chap_keyword[0] != 'chapter':
-            #     temp.append(i)
-            # else:
-            #     fresult2.append(" ".join(temp))
-            #     temp.clear()
-        # fresult_temp = " ".join(fresult)
-        # if re.search(r'chapter',fresult_temp):
+        
         temp = " ".join(fresult).split('chapter')
         for i in temp:
-            # print(i)
             fresult2.append("chapter"+i)
-        # else:
-        #     fresult2 = fresult
+  
         fresult2.pop(0)
             
         return fresult2
 
-    # for i in fresult:
-    #     chap_keyword = i.split(' ')
-    #     if chap_keyword[0] == 'chapter':
-    # result = clean_list()
-            # break
-        # else: 
-            # continue
     formatted_data = clean_list()
     for i in formatted_data:
         result.append(i[0:40])
@@ -50,4 +32,3 @@ def read_data(filepath):
         print(i)
         print("\n")
     return result
-read_data("demo2.jpg")
