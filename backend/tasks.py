abbc73f043dad8c62554d977b1ce443c107bf8e8
AtmegaBuzz <swapnilshinde9382@gmail.com>
2022-11-06 13:36:30 +0530

    Fix models name changes | update in views

--- backend/tasks.py
+++ backend/tasks.py
@@ -1,6 +1,6 @@
 import schedule
 import time
-from backend.models import Task,Todo,User
+from backend.models import Book,Chapter,User
 from extensions import twilio_client,twilio_number
 
 def send_message(msg,phno):
@@ -11,17 +11,17 @@ def send_message(msg,phno):
                           )
 
 
-def reminder_task(task_id,user_id):
+def reminder_task(book_id,user_id):
 
     print("starting reminder")
 
     user = User.query.filter_by(id=user_id).first()
-    task = Task.query.filter_by(id=task_id).first()
+    book = Book.query.filter_by(id=book_id).first()
 
     if (
-        task==None or
-        task.is_canceled == True or
-        task.is_completed == True or
+        book==None or
+        book.is_canceled == True or
+        book.is_completed == True or
         user==None
         ):
         print("cancelling reminder.")
@@ -29,19 +29,19 @@ def reminder_task(task_id,user_id):
 
     user_phno = user.phone_number
 
-    todos_pending = Todo.query.filter_by(task=task_id,is_inprogress=True).all()
-    latest_todo = Todo.query.filter_by(task=task_id,is_completed=False).first()
-    if len(todos_pending)!=0:
-        # print(f"Dude get up you have {len(todos_pending)} task pending. You have to Complete {task.title}.")
+    chapters_pending = Chapter.query.filter_by(book=book_id,is_inprogress=True).all()
+    latest_chapter = Chapter.query.filter_by(book=book_id,is_completed=False).first()
+    if len(chapters_pending)!=0:
+        # print(f"Dude get up you have {len(todos_pending)} book pending. You have to Complete {book.title}.")
         send_message(
-                f"Dude get up you have {len(todos_pending)} task pending. You have to Complete {task.title}.",
+                f"Dude get up you have {len(chapters_pending)} book pending. You have to Complete {book.title}.",
                 user_phno
             )
 
-    elif latest_todo!=None:
+    elif latest_chapter!=None:
         # print(f"Hey Reminder! You have to read {latest_todo.title}. Small efforts can make big Impacts.")
         send_message(
-                f"Hey Reminder! You have to read {latest_todo.title}. Small efforts can make big Impacts.",
+                f"Hey Reminder! You have to read {latest_chapter.title}. Small efforts can make big Impacts.",
                 user_phno
             )
 
